1f01ff3a4c44d83406ff013f07a7ba46be0dd67f
Tomas Berggren <tombe691@student.liu.se>
2015-11-17 14:35:25 +0100

    added new files

--- Tid.java
+++ Tid.java
@@ -5,14 +5,19 @@ import javax.swing.*;
 
 public class Tid {
 
-  public static String klockan() {
-    String s = Calendar.getInstance().getTime().toString();
+  static String klockan() {
+    String s = Calendar.getInstance().getDate().toString();
+    //    System.out.println(s);
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    String t sdf.format(s); 
+    System.out.println("test "+t);
+   
     int i = s.indexOf(':');
     return s.substring(i-2, i+6);
   }
 
   // Testprogram
   public static void main (String[] arg) {
-    JOptionPane.showMessageDialog(null, "Klockan är: " + klockan());
+    JOptionPane.showMessageDialog(null, "Klockan är llll : " + klockan());
   }
 }
