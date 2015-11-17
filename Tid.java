7885146e72d40a745f4b7d4c6bc5940539d15588
tombe691 <tombe691@student.liu.se>
2015-11-17 09:02:54 +0100

    divided into separate files
    moved method to its own file

--- Tid.java
+++ Tid.java
@@ -1,18 +1,10 @@
 // Filen Tid.java
 
-import java.util.*;
+//import java.util.*;
 import javax.swing.*;
 
 public class Tid {
-
-  public static String klockan() {
-    String s = Calendar.getInstance().getTime().toString();
-    int i = s.indexOf(':');
-    return s.substring(i-2, i+6);
-  }
-
-  // Testprogram
   public static void main (String[] arg) {
-    JOptionPane.showMessageDialog(null, "Klockan är: " + klockan());
+    JOptionPane.showMessageDialog(null, "Klockan är: " + Klockan.klockan());
   }
 }
