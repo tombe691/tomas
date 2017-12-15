d496188ecf60dcba843bb85fb96d27e6b59faff1
Tomas Berggren <tombe691@student.liu.se>
2017-12-15 10:17:58 +0100

    new files

--- c++/trycatch.cpp
+++ c++/trycatch.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 
 double division(int a, int b) {
-   
+   if( b == 0 ) {
+      throw "Division by zero condition!";
+   }
    return (a/b);
 }
 
@@ -10,14 +12,15 @@ int main () {
    int x = 50;
    int y = 0;
    double z = 0;
+   cout << "skriv en siffra";
    cin >> y;
+   
    try {
       z = division(x, y);
       cout << z << endl;
    } catch (const char* msg) {
      cerr << msg << endl;
    }
-   cout << "try again?";
-   cin >> y;
+
    return 0;
 }
