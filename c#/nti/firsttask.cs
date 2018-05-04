af5e66e12c7f320a492282d0f0f4bf9461395aaa
Tomas Berggren <tombe691@student.liu.se>
2018-05-04 12:13:58 +0200

    added cs and html files

---
+++ c#/nti/firsttask.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+ 
+namespace First_c_sharp_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name; //Variable for storing string value
+ 
+            //Displaying message for entring value
+            Console.WriteLine("Please Enter Your Good Name");
+ 
+            //Accepting and holding values in name variable
+            name = Console.ReadLine();
+ 
+            //Displaying Output
+            Console.WriteLine("Welcome {0} in your first csharp program", name);
+ 
+            //Holding console screen
+            Console.ReadLine();
+        }
+    }
+}
